0cbce56d09cdc0c58e75938e951317d80d4e48ef
yanatere <lamakau123>
2022-04-28 10:09:18 +0700

    first commit

---
+++ app/Models/Transaction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'users_id',
+        'address',
+        'payment',
+        'total_price',
+        'shipping_price',
+        'status',
+    ];
+
+    public function users(){
+        return $this->belongsTo(User::class,'users_id','id');
+    }
+
+    public function transactions(){
+        return $this->belongsTo(TransactionItem::class,'transactions_id','id');
+    }
+}
