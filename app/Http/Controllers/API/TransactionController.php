446eb4d6eb79da44b8fa4c84fc4bec8cffabb809
yanatere <lamakau123>
2022-05-17 08:04:06 +0700

    API Checkout

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helper\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit',6);
+        $status = $request->input('status');
+
+        if ($id) {
+            $transaction = Transaction::with(['items.product'])->find($id);
+
+            if ($transaction) {
+                return ResponseFormatter::success(
+                    $transaction,
+                    'Data Transaksi Berhasil Diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data Transaksi Tidak Ada',
+                    404
+                );
+            }
+        }
+
+        $transaction = Transaction::with(['items.product'])->where('users.id', Auth::user()->id);
+
+        if ($status) {
+            $transaction->where('status', $status);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Data List Berhasil Diambil'
+        );
+    }
+}
